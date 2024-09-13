9cb85fff1fe105170f65813a1767e2af5c7dd570
Thanawin Suwannasri <papigonwin001@gmail.com>
2024-09-14 02:58:26 +0700

    feat: add fetchCourses

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -2,27 +2,29 @@
 import { useEffect, useRef, useState } from "react";
 import DropDown from "./DropDown";
 import { PiPlusSquareBold, PiCheckSquareBold } from "react-icons/pi";
+import { CourseProps, Instructor } from "@/models/CourseProps";
 
-export type Course = {
-  courseId: string;
-  name: string;
-  credit: number;
-  section: Array<number>;
-  day: Array<string>;
-  capacity: Array<number>;
-  maxCapacity: Array<number>;
-  instructor: Array<string>;
-  registered: Array<boolean>;
-};
+// export type Course = {
+//   courseId: string;
+//   name: string;
+//   credit: number;
+//   section: Array<number>;
+//   day: Array<string>;
+//   capacity: Array<number>;
+//   maxCapacity: Array<number>;
+//   instructor: Array<string>;
+//   registered: Array<boolean>;
+// };
 
-interface CourseProps {
-  course: Course;
-}
 
-export default function CourseCard({ course }: CourseProps) {
+
+
+
+export default function CourseCard({ course, sections }: CourseProps) {
   const [dropdown, setDropdown] = useState(false);
-  const [section, setSection] = useState(0);
-  const [registered, setRegistered] = useState(course.registered[section]);
+  const [sectionIndex, setSectionIndex] = useState(0);
+  // const [registered, setRegistered] = useState(course.registered[section]);
+  const [registered, setRegistered] = useState(false);
 
   const dropdownRef = useRef<HTMLDivElement | null>(null);
 
@@ -43,20 +45,22 @@ export default function CourseCard({ course }: CourseProps) {
   }, []);
 
   useEffect(() => {
-    setRegistered(course.registered[section]);
-  }, [section]);
+    // setRegistered(course.registered[section]);
+  }, [sectionIndex]);
+
+  useEffect(() => {}, []);
 
   return (
     <div
       className={`flex flex-row m-auto w-[60%] ${
         dropdown ? "bg-[#979797]" : "bg-white"
-      } item-center h-12 hover:bg-[#979797]`}
+      } item-center h-20 hover:bg-[#979797]`}
     >
       <div className="flex w-[18%] text-bold justify-center text-black items-center">
-        {course.courseId}
+        {course.id}
       </div>
       <div className="flex w-[25%] text-bold justify-center text-black items-center">
-        {course.name}
+        {course.course_name}
       </div>
       <div className="flex w-[7%] text-bold justify-center text-black items-center">
         {course.credit}
@@ -68,30 +72,37 @@ export default function CourseCard({ course }: CourseProps) {
         }}
       >
         <DropDown
-          course={course.section}
+          // course={course.section}
+          course={sections ? sections.map((section) => section.section) : []}
           dropdown={dropdown}
           setDropDown={setDropdown}
-          section={section}
-          setSection={setSection}
+          section={sectionIndex}
+          setSection={setSectionIndex}
         />
       </button>
-      <div className="flex w-[15%] text-bold justify-center text-black items-center">
-        <div>{course.day[section]}</div>
+      <div className="flex w-[15%] text-bold justify-center text-black items-center flex-row">
+        <div>
+          {sections && sections[sectionIndex] && sections[sectionIndex].timeslots && sections[sectionIndex].timeslots.map((timeslot: any) => (<div key={timeslot[0]}>{timeslot}</div>)) }
+        </div>
       </div>
       <div className="flex w-[10%] text-bold justify-center text-black items-center">
-        {course.capacity[section]}/{course.maxCapacity[section]}
+        {sections && sections[sectionIndex] && sections[sectionIndex].capacity}/
+        {sections && sections[sectionIndex] && sections[sectionIndex].capacity}
       </div>
       <div className="flex w-[10%] text-bold justify-center text-black items-center">
-        {course.instructor[section]}
+        {sections &&
+          sections[sectionIndex] && sections[sectionIndex].instructors &&
+          sections[sectionIndex].instructors.map((i: Instructor) => i.display_name).join(', ')}
       </div>
       <button
         className="flex w-[8%] text-bold justify-center text-black items-center"
         onClick={() => {
           setRegistered(!registered);
-          course.registered[section] = !course.registered[section];
+          // course.registered[section] = !course.registered[section];
         }}
       >
-        {course.registered[section] ? (
+        {/* {course.registered[section] ? ( */}
+        {course ? (
           <PiCheckSquareBold size={32} />
         ) : (
           <PiPlusSquareBold size={32} />
--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -1,23 +1,71 @@
-import CourseCard, { Course } from "./CourseCard";
+"use client";
+import { Course, Section, CourseProps } from "@/models/CourseProps";
+import CourseCard from "./CourseCard";
 import CourseHeader from "./CourseHeader";
+import { useEffect, useState } from "react";
 
-const course: Course = {
-  courseId: "2110413",
-  name: "COMPUTER SECURITY",
-  credit: 3,
-  section: [1, 2, 3],
-  day: ["TUE 13:00-16:00", "WED 13:00-16:00", "THU 13:00-16:00"],
-  capacity: [40, 30, 20],
-  maxCapacity: [50, 50, 50],
-  instructor: ["KPR", "AAA", "BBB"],
-  registered: [false, false, false],
+// const course: Course = {
+//   courseId: "2110413",
+//   name: "COMPUTER SECURITY",
+//   credit: 3,
+//   section: [1, 2, 3],
+//   day: ["TUE 13:00-16:00", "WED 13:00-16:00", "THU 13:00-16:00"],
+//   capacity: [40, 30, 20],
+//   maxCapacity: [50, 50, 50],
+//   instructor: ["KPR", "AAA", "BBB"],
+//   registered: [false, false, false],
+// };
+type CourseData = {
+  course: Course;
+  sections: Section[];
 };
+async function getCoursesPaginated(page: number) {
+  try {
+    const response = await fetch(
+      `http://127.0.0.1:8080/api/v1/courses/paginated`,
+      {
+        method: "GET",
+        credentials: "include",
+      }
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching paginated courses:", error);
+    throw new Error("Failed to fetch paginated courses");
+  }
+}
 
 export default function CourseList() {
+  const [courses, setCourses] = useState<CourseData[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      setLoading(true);
+      try {
+        const data = await getCoursesPaginated(1);
+        setCourses(data.courses);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCourses();
+  }, []);
+
   return (
     <>
       <CourseHeader />
-      <CourseCard course={course} />
+      {courses.map((courseProps: CourseProps) => (
+        <CourseCard
+          key={courseProps.course.id}
+          course={courseProps.course}
+          sections={courseProps.sections}
+        />
+      ))}
     </>
   );
 }
