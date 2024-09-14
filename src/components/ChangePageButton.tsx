1dcd452456e55d26afd8045340a73ecc41e15275
WinSukon <jakrinsukon@gmail.com>
2024-09-15 00:05:51 +0700

    feat: complete navbar

--- src/components/ChangePageButton.tsx
+++ src/components/ChangePageButton.tsx
@@ -1,13 +1,19 @@
+'use client'
+import { useRouter } from "next/navigation";
+
 const ChangePageButton = ({
   pageName,
   active,
+  url
 }: {
   pageName: string;
   active: boolean;
+  url:string
 }) => {
+    const router = useRouter();
   return (
     <div className="p-1 m-1">
-      <button className="normal-text text-white">{pageName}</button>
+      <button className="normal-text text-white" onClick={()=>router.push('/'+url)}>{pageName}</button>
       {active ? <hr></hr> : null}
     </div>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,52 @@
+"use client";
+import { useState } from "react";
 import ChangePageButton from "./ChangePageButton";
 import ProfilePic from "./ProfilePic";
-
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 const NavBar = () => {
-    return ( 
+  const [isClicked, setISClicked] = useState<boolean>(false);
+  const pathname = usePathname();
+  return (
     <div className="w-full h-full bg-black flex flex-row items-center justify-between px-5">
-        <div className="flex flex-row">
-            <ChangePageButton pageName="Search for courses" active={true}></ChangePageButton>
-            <ChangePageButton pageName="Course Register" active={true}></ChangePageButton>
-            <ChangePageButton pageName="My Schedule" active={true}></ChangePageButton>
-            <ChangePageButton pageName="Registration History" active={true}></ChangePageButton>
+      <div className="flex flex-row">
+        <ChangePageButton
+          pageName="Search for courses"
+          active={pathname==='/search'}
+          url="search"
+        ></ChangePageButton>
+        <ChangePageButton
+          pageName="Course Register"
+          active={pathname==='/course-register'}
+          url="course-register"
+        ></ChangePageButton>
+        <ChangePageButton
+          pageName="My Schedule"
+          active={pathname==='/schedule'}
+          url="schedule"
+        ></ChangePageButton>
+        <ChangePageButton
+          pageName="Registration History"
+          active={pathname==='/history'}
+          url="history"
+        ></ChangePageButton>
+      </div>
 
-        </div>
-        <ProfilePic width={40} height={40}/>
-    </div> );
-}
- 
+      <div onClick={() => setISClicked(!isClicked)} className="size-[40px] ">
+        <ProfilePic width={40} height={40} />
+        {isClicked ? (
+          <div className="fixed z-10 normal-text text-center right-0  bg-[#D9D9D9] flex flex-col justify-center items-center w-32 m-5 divide-y-2 divide-black  border-2 border-black rounded-lg">
+            <div className="w-full p-1 ">
+              <Link href={"/profile"} className="hover:text-gray-600">
+                View Profile
+              </Link>
+            </div>
+            <div className="w-full p-1 ">Log out</div>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
 export default NavBar;
