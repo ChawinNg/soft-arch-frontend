7fd1e80c697cc90da3e597c9e6389c5e078134f8
ChawinNg <ik.chawin@gmail.com>
2024-10-17 23:31:04 +0700

    feat: notification v1

---
+++ src/components/Notification.tsx
@@ -0,0 +1,7 @@
+export default function Notification() {
+  return (
+    <div className="fixed right-4 bottom-8 bg-white py-4 px-6 animate-slide-in-out rounded-xl font-bold">
+      Your registration have been save
+    </div>
+  );
+}
