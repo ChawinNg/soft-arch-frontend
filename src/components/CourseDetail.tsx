6192949fe04b0c7c60364508f63f663f85e6e03a
WinSukon <jakrinsukon@gmail.com>
2024-10-20 15:18:11 +0700

    feat: send email

--- src/components/CourseDetail.tsx
+++ src/components/CourseDetail.tsx
@@ -2,13 +2,49 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-import { Course, Section } from "@/models/CourseProps";
+import { Course, Section, Instructor } from "@/models/CourseProps";
 import SectionHeader from "./SectionHeader";
 import SectionCard from "./SectionCard";
+import SendEmailModal from "./SendEmailModal";
+
+function getUniqueInstructors(data: Section[]): Instructor[] {
+  const uniqueInstructorsMap = new Map<number, Instructor>();
+
+  // Check if data is an array and has sections
+  if (!Array.isArray(data)) {
+      console.error('Expected data to be an array of sections');
+      return [];
+  }
+
+  data.forEach((section) => {
+      // Check if the section has instructors and that it is an array
+      if (section.instructors && Array.isArray(section.instructors)) {
+          section.instructors.forEach((instructor) => {
+              if (instructor && instructor.instructor_id) {
+                  console.log(instructor);
+                  uniqueInstructorsMap.set(instructor.instructor_id, instructor);
+              } else {
+                  console.warn('Invalid instructor object', instructor);
+              }
+          });
+      } else {
+          console.warn('Instructors is not an array or is missing for section', section);
+      }
+  });
+
+  console.log("Unique Instructors Map:", uniqueInstructorsMap);
+  // Convert the Map values back to an array
+  return Array.from(uniqueInstructorsMap.values());
+}
+
 
 export default function CourseDetail() {
   const [course, setCourse] = useState<Course | null>();
-  const [sections, setSections] = useState<Section[] | null>();
+  const [sections, setSections] = useState<Section[]>([] as Section[]);
+  const [isContacting, setIsContacting] = useState<boolean>(false);
+  const [instructors, setInstructors] = useState<Instructor[]>(
+    [] as Instructor[]
+  );
   const params = useParams<{ id: string }>();
 
   useEffect(() => {
@@ -18,6 +54,8 @@ export default function CourseDetail() {
         const sectionData = await getSectionsFromCourseId(params.id);
         setCourse(courseData);
         setSections(sectionData);
+        const inst:Instructor[] = getUniqueInstructors(sectionData);
+        setInstructors(inst);
       } catch (error) {
         console.error("Error fetching course detail: ", error);
         throw new Error("Failed to fetch course detail");
@@ -27,40 +65,53 @@ export default function CourseDetail() {
   }, []);
 
   return (
-    <div className="w-[90%] flex flex-col bg-white rounded-b-lg h-full p-8 md:p-16">
-      <div className="flex flex-col">
-        <Link
-          href={`/`}
-          className="bg-black py-2 md:py-3 text-white font-bold rounded-md relative w-24 md:w-28 flex justify-center"
-        >
-          Back
-        </Link>
+    <>
+      <SendEmailModal
+        isOpen={isContacting}
+        onClose={() => setIsContacting(false)}
+        instructors={instructors}
+      />
+      <div className="w-[90%] flex flex-col bg-white rounded-b-lg h-full p-8 md:p-16">
+        <div className="flex flex-col">
+          <Link
+            href={`/`}
+            className="bg-black py-2 md:py-3 text-white font-bold rounded-md relative w-24 md:w-28 flex justify-center"
+          >
+            Back
+          </Link>
 
-        {course && (
-          <>
-            <div className="font-bold flex flex-col md:flex-row justify-between items-center mt-4">
-              <div className="text-xl md:text-2xl">
-                {course.id} {course.course_name} {`[${course.credit} Credit]`}
+          {course && (
+            <>
+              <div className="font-bold flex flex-col md:flex-row justify-between items-center mt-4">
+                <div className="text-xl md:text-2xl">
+                  {course.id} {course.course_name} {`[${course.credit} Credit]`}
+                </div>
+
+                <button
+                  onClick={() => {
+                    setIsContacting(true);
+                    console.log(instructors);
+                  }}
+                  className="bg-black py-2 md:py-3 px-4 mt-4 md:mt-0 text-white font-bold rounded-md flex justify-center"
+                >
+                  Contact Instructor
+                </button>
               </div>
 
-              <button className="bg-black py-2 md:py-3 px-4 mt-4 md:mt-0 text-white font-bold rounded-md flex justify-center">
-                Contact Instructor
-              </button>
-            </div>
+              <div className="mt-6 text-lg">
+                <div className="font-bold">Course Description</div>
+                <div className="mt-2">{course.description}</div>
+              </div>
 
-            <div className="mt-6 text-lg">
-              <div className="font-bold">Course Description</div>
-              <div className="mt-2">{course.description}</div>
-            </div>
-
-            <SectionHeader />
-            {sections?.map((section) => (
-              <SectionCard section={section} />
-            ))}
-          </>
-        )}
+              <SectionHeader />
+              {sections?.map((section) => (
+                <SectionCard section={section} />
+              ))}
+            </>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/SendEmailModal.tsx
+++ src/components/SendEmailModal.tsx
@@ -1,25 +1,105 @@
+import { useAuth } from "@/context/AuthProvider";
+import { Instructor } from "@/models/CourseProps";
 import { useState } from "react";
+
+type Email = {
+  from_name: string;
+  to_email: string;
+  header: string;
+  body: string;
+};
+
+export async function sendEmail(email: Email) {
+  try {
+    const response = await fetch(
+      "http://localhost:8080/api/v1/instructors/contact",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify(email),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("can't send email");
+    }
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 export default function SendEmailModal({
   isOpen,
   onClose,
-  recipient,
+  instructors,
 }: {
   isOpen: boolean;
-  onClose?: () => void;
-  recipient: string;
+  onClose: () => void;
+  instructors: Instructor[];
 }) {
+  const [headerMessage, setHeaderMessage] = useState("");
   const [message, setMessage] = useState("");
-
+  const [selectedInstructor, setSelectedInstructor] = useState<Instructor>(
+    instructors[0]
+  );
+  const { user } = useAuth();
   if (!isOpen) return null;
 
-  const handleSend = () => {
+  const handleSend = async () => {
+    const email: Email = {
+      from_name: user.name + " " + user.surname,
+      to_email: selectedInstructor.email
+        ? selectedInstructor.email
+        : "jakrinsukon@gmail.com", //change this to your email
+      header: headerMessage,
+      body: message,
+    };
+    const res = await sendEmail(email);
+    console.log(res);
+    onClose();
+  };
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedId = Number(event.target.value);
+    const inst = instructors.find(
+      (instructor) => instructor.instructor_id === selectedId
+    );
+    setSelectedInstructor(inst!);
   };
 
   return (
     <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center z-50 ">
       <div className="bg-white rounded-lg w-full max-w-md p-6 shadow-lg relative">
-        <h2 className="text-2xl font-semibold mb-4">Send to: {recipient}</h2>
+        <h2 className="text-2xl font-semibold mb-4">
+          Send to:
+          <select onChange={handleSelectChange}>
+            {instructors.map((instructor) => (
+              <option
+                key={instructor.instructor_id}
+                value={instructor.instructor_id}
+              >
+                {instructor.display_name}
+              </option>
+            ))}
+          </select>
+        </h2>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Header
+          </label>
+          <input
+            type="text"
+            value={headerMessage}
+            onChange={(e) => setHeaderMessage(e.target.value)}
+            className="w-full border border-gray-300 p-2 rounded-lg focus:outline-none focus:ring focus:border-black"
+            placeholder="Write your header here..."
+          />
+        </div>
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2">
             Message
